300e0456711920f4cf92b392f603bc1ede8d61c2
aleshasulahry-hub <aleshasulahry@gmail.com>
2026-04-09 13:26:37 -0500

    Add motivational quotes to support user wellness and growth

---
+++ script.js
@@ -0,0 +1,9 @@
+const quotes = [
+  "You are growing beautifully, even on slow days.",
+  "Small steps still count.",
+  "Rest is productive too.",
+  "You deserve softness and strength.",
+  "Be kind to yourself today.",
+  "Softness and consistency can exist together.",
+  "Progress matters more than perfection."
+];
